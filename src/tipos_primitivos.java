3c59695f473063d3c33a0ae25ecd8dc2a7a37bf5
TassioSales <tassio.ljs@gmail.com>
2021-07-03 02:04:44 -0300

    commit inicial

---
+++ src/tipos_primitivos.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.SynchronousQueue;
+
+public class tipos_primitivos {
+    public static void main(String[] args){
+        byte a;
+        byte a1 = 127;
+        byte a2 = 30;
+
+        char a3;
+        char a4 = 'A';
+        char a5 = 15;
+
+        short a6;
+        short a7 = 23232;
+        short a8 = 4554;
+
+        int a9;
+        int a10 = 122112;
+
+        long a11 = 1212121212;
+        long a12 = 1112221111;
+
+        double a13 = 2.5656564;
+        double a14 = 1.23434;
+
+        float a15  = 2;
+        float a16 = 565;
+
+        boolean a17 = true;
+        boolean a18 = false;
+
+        System.out.println(a4);
+
+    }
+
+}
